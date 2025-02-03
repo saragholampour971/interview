64cf9da9a3037954a2873caf0426f78ee79666c9
Sara <saragholampour971@yahoo.com>
2025-02-03 20:11:02 +0330

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ScrollableArea from "./components/scrollable-area";
+
+function App() {
+  return (
+    <div className="App">
+        <ScrollableArea/>
+    </div>
+  );
+}
+
+export default App;
